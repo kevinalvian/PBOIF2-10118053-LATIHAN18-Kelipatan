eccb217e7188fff2fa0e754ccd393b45ba94f1eb
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-10-10 13:23:39 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118053/latihan18/kelipatan/PBOIF210118053LATIHAN18Kelipatan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118053.latihan18.kelipatan;
+
+/**
+ *
+ * @author ACER
+ */
+public class PBOIF210118053LATIHAN18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5) {
+                System.out.println(i);
+    }
+    
+}
